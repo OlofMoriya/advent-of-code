09c5b6cb9b83e5736edb1b9e6bd4d8317247426a
OlofMoriya <olor.moriya@gmail.com>
2022-12-05 08:52:18 +0100

    bash for 01.

--- src/day_01/01.sh
+++ src/day_01/01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cat "./input/22_01_test" | sed -z 's/\n/+/g' | sed -z 's/++/\n/g' | bc
+cat "../../input/22_01_test" | sed -z 's/\n/+/g' | sed -z 's/++/\n/g' | sed -z 's/.$/\n/' | bc | sort -n | tail -n 1
 
+cat "../../input/22_01_test" | sed -z 's/\n/+/g' | sed -z 's/++/\n/g' | sed -z 's/.$/\n/' | bc | sort -n | tail -n 3 |sed -z 's/\n/+/g' | sed -z 's/.$/\n/' | bc
